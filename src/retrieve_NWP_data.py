e7cd276f196e94e70671f00ded63fb6a31280e9e
princekx <prince.xavier@gmail.com>
2022-03-31 20:13:07 +0100

    new function to extract GPM for the same time bounds

--- src/retrieve_NWP_data.py
+++ src/retrieve_NWP_data.py
@@ -1,9 +1,106 @@
 import os
 import sys
 import datetime
-import numpy as np
+import iris
+from iris.time import PartialDateTime
+import glob
 
-def retrieve_nwp_3hr_data(date, hour, lead, moosedir=None):
+
+def regrid_cube(my_cubes, base_cube):
+    base_cube.coord('latitude').units = my_cubes.coord('latitude').units = 'degrees'
+    base_cube.coord('longitude').units = my_cubes.coord('longitude').units = 'degrees'
+
+    base_cube.coord('latitude').coord_system = None
+    base_cube.coord('longitude').coord_system = None
+    my_cubes.coord('latitude').coord_system = None
+    my_cubes.coord('longitude').coord_system = None
+
+    # For lat/lon regriding, make sure coordinates have bounds
+    if my_cubes.coord('longitude').bounds is None:
+        my_cubes.coord('longitude').guess_bounds()
+    if my_cubes.coord('latitude').bounds is None:
+        my_cubes.coord('latitude').guess_bounds()
+    if base_cube.coord('longitude').bounds is None:
+        base_cube.coord('longitude').guess_bounds()
+    if base_cube.coord('latitude').bounds is None:
+        base_cube.coord('latitude').guess_bounds()
+
+    #reg_cube = iris.experimental.regrid.regrid_area_weighted_rectilinear_src_and_grid(my_cubes, \
+    #                                                                                  base_cube, mdtol=0)
+    reg_cube = my_cubes.regrid(base_cube, iris.analysis.Linear())
+    return reg_cube
+
+
+def retrieve_gpm_from_modelTimeBounds(date, hour=None, lead=None, model_data_dir=None,
+                                      gpm_folder='/project/earthobs/PRECIPITATION/GPM/netcdf/imerg/NRTearly/1hr-accum',
+                                      gpm_out_folder='/scratch/hadpx/Feature_monitor_data/gpm'):
+    '''
+    Generate the GPM data to match the model data
+    :param date:
+    :type date:
+    :param hour:
+    :type hour:
+    :param lead:
+    :type lead:
+    :param model_data_dir:
+    :type model_data_dir:
+    :param gpm_folder:
+    :type gpm_folder:
+    :return:
+    :rtype:
+    '''
+    str_year, str_month, str_day = str(date.year), str('%02d' % date.month), str('%02d' % date.day)
+    date_label = '%s%s%s' % (str_year, str_month, str_day)
+
+    # read the model data
+    ##########################################################################
+    moose_file_name = 'prods_op_gl-mn_%s_%s_%s.pp' % (date_label, hour, lead)
+    remote_data_dir_outfile = os.path.join(model_data_dir, moose_file_name)
+    model_cube = iris.load_cube(remote_data_dir_outfile)
+    m_time_coord = model_cube.coord('time')
+    m_time_bounds = model_cube.coord('time').bounds
+    #m_cube_times = [m_time_coord.units.num2date(tp) for tp in m_time_coord.points]
+
+    # m_time_bounds as datetime objects from model
+    #time_delta = m_time_bounds[1] - m_time_bounds[0]
+    # time delta in hours to choose 3 hourly or 6 hourly mean GPM data
+    #time_delta = time_delta.seconds / 60 / 60.
+    #gpm_folder = os.path.join(gpm_folder, '%shr-accum' % str(int(time_delta)))
+    #
+    # read GPM data
+    file_name = glob.glob(gpm_folder + '/*.nc')[0]
+    print(file_name)
+    obs_cube = iris.load_cube(file_name, 'm01s05i216')
+
+    # now make a time constraint on gpm based on model data bounds
+    pdt1 = PartialDateTime(year=m_time_bounds[0].year,
+                           month=m_time_bounds[0].month,
+                           day=m_time_bounds[0].day,
+                           hour=m_time_bounds[0].hour)
+    pdt2 = PartialDateTime(year=m_time_bounds[1].year,
+                           month=m_time_bounds[1].month,
+                           day=m_time_bounds[1].day,
+                           hour=m_time_bounds[1].hour)
+    print(pdt1, pdt2)
+    # Constrain the time
+    gpm_time_constraint = iris.Constraint(time=lambda cell: pdt1 <= cell.point < pdt2)
+    obs_cube = obs_cube.extract(gpm_time_constraint)
+
+    if len(obs_cube.shape) == 3:
+        obs_cube = obs_cube.collapsed('time', iris.analysis.SUM)
+
+    # extracting SEAsia
+    model_cube = model_cube.intersection(latitude=(-20, 30), longitude=(100, 130))
+    obs_cube = obs_cube.intersection(latitude=(-20, 30), longitude=(100, 130))
+
+    # regridding obs to model grid
+    obs_cube = regrid_cube(obs_cube, model_cube)
+
+    obs_outfile = os.path.join(gpm_out_folder, 'gpm_%s_%s_%s.nc' %(date_label, hour, lead))
+    #return obs_cube
+
+
+def retrieve_nwp_3hr_data(date, hour=None, lead=None, moosedir=None, remote_data_dir=None):
     '''
     Retrieve NWP global 3hourly data
 
@@ -25,7 +122,7 @@ def retrieve_nwp_3hr_data(date, hour, lead, moosedir=None):
     query_files_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'queryfiles')
 
     print(query_files_dir)
-    remote_data_dir = '/scratch/hadpx/Feature_monitor_data/'
+
     if not os.path.exists(remote_data_dir):
         print('Making dir: %s' % remote_data_dir)
         os.makedirs(remote_data_dir)
@@ -58,83 +155,6 @@ def retrieve_nwp_3hr_data(date, hour, lead, moosedir=None):
         print(command)
         os.system(command)
 
-def retrieve_nwp_forecast_data(date, runid=None, moosedir=None):
-    str_year, str_month, str_day = str(date.year), str('%02d' % date.month), str('%02d' % date.day)
-
-    date_label = '%s%s%s' % (str_year, str_month, str_day)
-    print('Doing date : %s' % date_label)
-    query_files_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'queryfiles')
-
-    print(query_files_dir)
-    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # !!!!!! BUG ALERT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # Bug in MOGREPS data archival. It only archives 12 members. 
-    # Correct this once the full 36 members are available on moose.
-    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # This is the correct path for prod run
-
-    # Use this for temporary use for PS39 bug
-    # moosedir = data_paths.dirs('mog_moose_dir')# + '%s%s.pp' % (str_year, str_month)
-
-    hr_list = [00]
-    fc_times = np.arange(0, 150, 6)  # 15 days
-    print(fc_times)
-
-    # Loop over hour
-
-    for hr in hr_list:
-        remote_data_dir = os.path.join('/scratch/hadpx/Feature_montor_data/%s/%s/%s/%s/%s' % (runid,
-                                                                                              str_year, str_month,
-                                                                                              str_day,
-                                                                                              str('%02d' % hr)))
-
-        if not os.path.exists(remote_data_dir):
-            print('Making dir: %s' % remote_data_dir)
-            os.makedirs(remote_data_dir)
-            # os.system('ls %s' %remote_data_dir)
-
-        # os.system('moo ls %s' %file_moose)
-        precip_query_file = os.path.join(query_files_dir, 'mogg_query_precip')
-        local_query_file1 = os.path.join(query_files_dir, 'local_query1')
-
-        hr_str = str('%02d' % hr)
-        # check if the directory is empty
-        for fc in fc_times:
-            fc_3d = str('%03d' % fc)
-            print('Forecast is %s ' % fc)
-            # filemoose = 'prods_op_mogreps-g_%s%s%s_%s_%s_%s.pp' % (str_year, str_month, str_day, hr, digit2_mem, fct)
-            # filemoose_pb = '%s%s%sT%s00Z_pb_t%s.pp' % (str_year, str_month, str_day, hr_str, fc_3d)
-            filemoose_pb = '%s%s%sT%s00Z_glm_t%s.ff' % (str_year, str_month, str_day, hr_str, fc_3d)
-            outfile = 'englaa_pb%s.pp' % fc_3d
-            file_moose_pb = os.path.join(moosedir, filemoose_pb)
-            print(file_moose_pb)
-            try:
-                # Replace the fctime and filemoose in query file
-                replacements = {'fctime': str(fc), 'filemoose_pb': filemoose_pb}
-
-                with open(precip_query_file) as query_infile, open(local_query_file1, 'w') as query_outfile:
-                    for line in query_infile:
-                        for src, target in replacements.items():
-                            line = line.replace(src, target)
-                        query_outfile.write(line)
-
-                # do the retrieval
-                remote_data_dir_outfile = '%s/%s' % (remote_data_dir, outfile)
-
-                if not os.path.exists(remote_data_dir_outfile):
-                    command = '/opt/moose-client-wrapper/bin/moo select %s %s %s' % (local_query_file1,
-                                                                                     moosedir,
-                                                                                     remote_data_dir_outfile)
-                    print(command)
-                    os.system(command)
-                else:
-                    print('%s found. Skipping retrieval...' % remote_data_dir_outfile)
-            except:
-                print('%s not returned. Check file on moose' % file_moose)
-                sys.exit()
-
-    else:
-        print('%s has files. Skipping retrieval...' % remote_data_dir)
 
 
 if __name__ == '__main__':
@@ -142,4 +162,10 @@ if __name__ == '__main__':
     yesterday = today - datetime.timedelta(days=1)
     yesterday = datetime.date(2022,3,29)
     moosedir = 'moose:/opfc/atm/global/prods/'
-    retrieve_nwp_3hr_data(yesterday, hour='06', lead='003', moosedir=moosedir)
+    model_data_dir = '/scratch/hadpx/Feature_monitor_data/global_um'
+
+    retrieve_nwp_3hr_data(yesterday, hour='06', lead='009',
+                          moosedir=moosedir, remote_data_dir=model_data_dir)
+
+    obs_data_dir = '/scratch/hadpx/Feature_monitor_data/gpm'
+    retrieve_gpm_from_modelTimeBounds(yesterday, hour='06', lead='009', model_data_dir=obs_data_dir)
